ec8fe26eef3766fc189abe4e4acd874a89741cd7
RoshanManandhar <roshanmanandhar750@gmail.com>
2026-05-31 18:25:08 +0545

    hello

---
+++ day1.py
@@ -0,0 +1,82 @@
+print("Hello world")
+
+name ="ram"
+faculty="Computer Science"
+dob="17/05/2061"
+
+# string concatenation
+name = input("Enter your name: ")
+faculty = input("Enter your faculty: ")
+dob = input("Enter your DOB: ")
+address = input("Enter your address: ")
+
+print("Hello, my name is: " + name)
+print("My faculty is: " + faculty)
+print("My DOB is: " + dob)
+print("My address is: " + address)
+
+#check data types
+print("Type of name:", type(name))
+print("Type of faculty:", type(faculty))
+print("Type of dob:", type(dob))
+print("Type of address:", type(address))
+
+#multiple assignment
+name, faculty, dob, roll_no = "Roshan", "BCA", "2061-05-17", 12345
+
+#dta types
+print(type(name))
+print(type(faculty))
+print(type(dob))
+print(type(roll_no))
+
+#value swapping
+a = 10
+b = 20
+print("before swapping: a =", a,"b =", b)
+a, b =b, a
+print("before swapping: a =", a,"b =", b)
+
+#unpack list
+my_list = ["ram", 20, 30.0]
+
+name, *other = my_list
+
+print(my_list)
+name, age, height = my_list
+
+print("Name:",name )
+print("Age:",age )
+print("Height:",height )
+
+
+#creating class
+student_name = ["Alice", "Bob", "Charlie", "Daniya"]
+student_id = [1, 2, 3, 4]
+print(student_name)
+print(student_id)
+
+print(student_name[0])  # Alice
+print(student_name[1])  # Bob
+print(student_name[2])  # Charlie
+print(student_name[3])  # Daniya
+
+#accesing element in list with indexing (0-n)
+list1= [1, 2, 3, 4, 5]
+print(list1[0])
+print(list1[0:3])
+print("Even position:", list1[::2])
+print("Odd position:", list1[1::2])
+
+#list comprehension
+passing_marks = [45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
+
+passing_marks = [mark for mark in passing_marks if mark <= 80]
+print("Pass student: ",passing_marks)
+
+#common methods
+print(len(passing_marks))
+print(max(passing_marks))
+print(min(passing_marks))
+print(sum(passing_marks))
+ 
