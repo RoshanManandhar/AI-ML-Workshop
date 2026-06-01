7eb67dde75c87291590f43d9cec5ec8a1aa389ea
RoshanManandhar <roshanmanandhar750@gmail.com>
2026-06-01 07:22:34 +0545

    hello

--- day1.py
+++ day1.py
@@ -59,51 +59,62 @@
 # print(student_name[0])  # Alice
 # print(student_name[1])  # Bob
 # print(student_name[2])  # Charlie
-# print(student_name[3])  # Daniya
+# # print(student_name[3])  # Daniya
 
-# #accesing element in list with indexing (0-n)
-# list1= [1, 2, 3, 4, 5]
-# print(list1[0])
-# print(list1[0:3])
-# print("Even position:", list1[::2])
-# print("Odd position:", list1[1::2])
+# # #accesing element in list with indexing (0-n)
+# # list1= [1, 2, 3, 4, 5]
+# # print(list1[0])
+# # print(list1[0:3])
+# # print("Even position:", list1[::2])
+# # print("Odd position:", list1[1::2])
 
-# #list comprehension
-# passing_marks = [45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
+# # #list comprehension
+# # passing_marks = [45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
 
-# passing_marks = [mark for mark in passing_marks if mark <= 80]
-# print("Pass student: ",passing_marks)
+# # passing_marks = [mark for mark in passing_marks if mark <= 80]
+# # print("Pass student: ",passing_marks)
 
-# #common methods
-# print(len(passing_marks))
-# print(max(passing_marks))
-# print(min(passing_marks))
-# print(sum(passing_marks))
+# # #common methods
+# # print(len(passing_marks))
+# # print(max(passing_marks))
+# # print(min(passing_marks))
+# # print(sum(passing_marks))
  
-#Tuples cannot be changed after creation
-# student_record = ["Alice", 20, 85.5, "Computer Science"]
-# print("Student Record Tuple",student_record)
+# #Tuples cannot be changed after creation
+# # student_record = ["Alice", 20, 85.5, "Computer Science"]
+# # print("Student Record Tuple",student_record)
 
-# Accessing tuple elements
-# print("Name", student_record[0])
-# print("Age", student_record[1])
+# # Accessing tuple elements
+# # print("Name", student_record[0])
+# # print("Age", student_record[1])
 
-#set automatically remove duplicates
-course_A = {"Alice","Bob", "Charlie", "Diama"}
-course_B = {"Charlie","Diama", "Eve", "Franks"}
+# #set automatically remove duplicates
+# course_A = {"Alice","Bob", "Charlie", "Diama"}
+# course_B = {"Charlie","Diama", "Eve", "Franks"}
 
-print("Course A students", course_A)
-print("Course B students", course_B)
+# print("Course A students", course_A)
+# print("Course B students", course_B)
 
-#Set operationns (great for finding overlaps)
-print("\nStudents in both courses:", course_A & course_B)
-print("Students in either course:", course_A | course_B)
-print("Only in course A:", course_A - course_B)
-print("Only in one course:", course_A ^ course_B)
+# #Set operationns (great for finding overlaps)
+# print("\nStudents in both courses:", course_A & course_B)
+# print("Students in either course:", course_A | course_B)
+# print("Only in course A:", course_A - course_B)
+# print("Only in one course:", course_A ^ course_B)
 
-#Remove implicates from list using txt
-scores_with_implicates = [15, 32, 65, 75, 60, 70]
-unique_scores = list(set(scores_with_implicates))
+# #Remove implicates from list using txt
+# scores_with_implicates = [15, 32, 65, 75, 60, 70]
+# unique_scores = list(set(scores_with_implicates))
 
+print("\n --- Dictionaries ---")
+print("="*50)
 
+student = {
+    "name": "Alice",
+    "age": 20,
+    "scores": [55, 65, 75, 85],
+    "department": "Computer Science",
+    "is_active": True
+}
 
+print("Student Dictionary")
+print(student)
